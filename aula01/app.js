43fe8ade5dca8fcc47f9de18d7e43c584faaeeb9
Davifxm <davi1103felix@gmail.com>
2026-02-19 19:12:58 -0300

    Add files via upload

---
+++ aula01/app.js
@@ -0,0 +1,12 @@
+//1
+const pais = "Brasil";
+const nome = "Davi";
+let idade = 17;
+const estado = "pernanbuco";
+const cidade = "paulista";
+console.log(`Olá, me chamo ${nome}, tenho ${idade} anos, moro em ${cidade} - ${estado}, no país ${pais}.`);
+ // 7
+
+const num = [1, 8, 3, 12, 5];
+const filtrado = num.filter(num => num >= 7);
+document.writeln(filtrado);
