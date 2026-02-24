9ed76794c3a6be27632d5531abacb629543cef74
Aluno <davi1103felix@gmail.com>
2026-02-24 11:00:08 -0300

    feat: setup react vite tsx project aula05

---
+++ aula05/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function App() {
+  const [price, setPrice] = useState<number>(10);
+  const [qty, setQty] = useState<number>(2);
+
+  function calcTotal() {
+    debugger;
+    const total = price + qty;
+    return total;
+  }
+
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Debug no React + Vite + TS</h1>
+
+      <p>Preço (number): {price}</p>
+      <p>Quantidade (number): {qty}</p>
+
+      <button onClick={() => console.log("Total:", calcTotal())}>
+        Calcular total
+      </button>
+      
+
+    </div>
+
+  );
+
+}
